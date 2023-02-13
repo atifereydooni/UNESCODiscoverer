559b6d213b81974e823d461407403db23c71fcbb
atifereydooni <am.fereydooni@gmail.com>
2023-02-14 00:29:07 +0100

    Add buildSrc for the gradle management

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,17 @@
+object Dependencies {
+    const val androidxCore = "androidx.core:core-ktx:${Versions.androidxCore}"
+    const val androidxLifecycle = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.androidxLifecycle}"
+
+    //compose
+    const val activityCompose = "androidx.activity:activity-compose:${Versions.activityCompose}"
+    const val composeUI = "androidx.compose.ui:ui:${Versions.compose}"
+    const val composeUiTooling = "androidx.compose.ui:ui-tooling-preview:${Versions.compose}"
+    const val composeMaterial = "androidx.compose.material:material:${Versions.composeMaterial}"
+    const val composeNavigation = "androidx.navigation:navigation-compose:${Versions.composeNavigation}"
+    const val accompanistNavigation = "com.google.accompanist:accompanist-navigation-animation:${Versions.accompanistNavigation}"
+
+    // testing
+    const val jUnitTest = "junit:junit:${Versions.jUnitTest}"
+    const val composeUiTest = "androidx.compose.ui:ui-test-junit4:${Versions.compose}"
+
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,16 @@
+object Versions {
+    const val kotlin = "1.6.10"
+
+    const val androidxCore = "1.9.0"
+    const val androidxLifecycle = "2.5.1"
+
+    //compose
+    const val activityCompose = "1.6.1"
+    const val compose = "1.3.3"
+    const val composeMaterial = "1.3.1"
+    const val composeNavigation = "2.5.3"
+    const val accompanistNavigation = "0.22.0-rc"
+
+    // testing
+    const val jUnitTest = "4.13.2"
+}
