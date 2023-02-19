bc364d1caf53fd1f6f93c8fc7ad2b506599fdbc7
atifereydooni <am.fereydooni@gmail.com>
2023-02-19 23:04:00 +0100

    Add a new module for the Home feature

--- app/src/main/java/com/polaroid/unescodiscoverer/navigation/NavHostGraph.kt
+++ app/src/main/java/com/polaroid/unescodiscoverer/navigation/NavHostGraph.kt
@@ -1,9 +1,9 @@
 package com.polaroid.unescodiscoverer.navigation
 
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavGraphBuilder
 import com.google.accompanist.navigation.animation.composable
+import com.polaroid.home.presentation.HomeScreen
 import com.polaroid.navigation.destination.HomeDestination
 import com.polaroid.navigation.destination.NavigationDestination
 
@@ -13,11 +13,6 @@ private fun composableDestinations(): Map<NavigationDestination, @Composable () 
             HomeScreen()
         }
     )
-
-@Composable
-fun HomeScreen() {
-    Text(text = "Home Screen")
-}
 
 fun NavGraphBuilder.addComposableDestinations() {
     composableDestinations().forEach { entry ->
---
+++ feature/home/src/main/java/com/polaroid/home/presentation/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.polaroid.home.presentation
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Text(text = "Home Screen")
+}
